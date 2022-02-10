3879a747cf3248aaf00a107072db48399f16d307
runhouse <99401789+runhouse@users.noreply.github.com>
2022-02-10 20:31:34 +0800

    changed

--- src/pages/new-page.jsx
+++ src/pages/new-page.jsx
@@ -1,6 +1,8 @@
 import * as React from "react"
 
 export default function Component() {
-  return "Hello world"
-  <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fdavid77up%2Fposts%2F10159954798540953&show_text=true&width=500" width="500" height="192" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+  return (<iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fdavid77up%2Fposts%2F10159954798540953&show_text=true&width=500" width="500" height="192" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+    
+  )
+  
 }
